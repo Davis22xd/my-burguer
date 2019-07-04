94d542397317d218b877e7ed06c30df8f627546f
David <david@Davids-MacBook-Pro-4.local>
2019-07-04 14:13:17 -0500

    create build version

---
+++ build/precache-manifest.557a3488325c4dae0a88b9e6eb28a3d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0ab8ba7144cd328a4ebdd1b7670772",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fedf6d96ea64623af1a",
+    "url": "/static/css/main.226fe3ca.chunk.css"
+  },
+  {
+    "revision": "71b554b20e6ad000845b",
+    "url": "/static/js/2.205bd07c.chunk.js"
+  },
+  {
+    "revision": "5fedf6d96ea64623af1a",
+    "url": "/static/js/main.93058093.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
